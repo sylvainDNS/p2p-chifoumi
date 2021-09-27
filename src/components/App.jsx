8d5f17c693e4b74d8abb16d5a550412b1fd4a093
Sylvain DENYSE <sylvain.denyse@knplabs.com>
2021-09-27 15:38:44 +0200

    build: init

---
+++ src/components/App.jsx
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled'
+
+const App = () => (
+  <Wrapper>
+    <Header>Chifoumi</Header>
+    <Main></Main>
+    <Footer>Nathan & Sylvain - 2021</Footer>
+  </Wrapper>
+)
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  height: 100%;
+`
+const Header = styled.header`
+  text-align: center;
+`
+const Main = styled.main`
+  flex: 1;
+`
+const Footer = styled.footer`
+  text-align: center;
+`
+export default App
